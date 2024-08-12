00ebf7cc1b3b09dff7b755ac3082834ae2c1f53d
sslegoale0 <sslegoale0@gmail.com>
2024-08-12 04:02:24 +0200

    Add files via upload

--- E-Commerce Sales Data Cleaning & Analysis (SQL Server).sql
+++ E-Commerce Sales Data Cleaning & Analysis (SQL Server).sql
@@ -176,13 +176,13 @@ SELECT DISTINCT [ZIP Code]
 FROM orders
 ORDER BY [ZIP Code];
 
-SELECT [ZIP Code]
+SELECT *
 FROM orders
-WHERE [ZIP Code] = '';
+WHERE [ZIP Code] IS NULL;
 
 UPDATE orders
 SET [ZIP Code] = [State]
-WHERE [ZIP Code] = '';
+WHERE [ZIP Code] IS NULL;
 
 UPDATE orders
 SET [State] = NULL
@@ -243,7 +243,6 @@ SELECT DISTINCT [Region]
 FROM orders
 ORDER BY [Region];
 
-
 SELECT DISTINCT [Market],
 [Region]
 FROM orders
@@ -283,7 +282,7 @@ FROM orders
 ORDER BY [Quantity] ASC;
 
 ALTER TABLE orders
-ALTER COLUMN [Quantity] INT;
+ALTER COLUMN [Quantity] FLOAT;
 
 
 
@@ -300,7 +299,7 @@ SELECT DISTINCT [Profit Margin]
 FROM orders
 ORDER BY [Profit Margin] ASC;
 
-SELECT [Profit Margin],
+SELECT DISTINCT [Profit Margin],
 CONVERT(FLOAT, REPLACE([Profit Margin], '%', ''))/100
 FROM orders;
 
@@ -316,8 +315,11 @@ SELECT DISTINCT [Profit Per Order]
 FROM orders
 ORDER BY [Profit Per Order] ASC;
 
+EXEC sp_rename 'orders.[Profit Per Order]', 'Profit', 'COLUMN';
+
+
 ALTER TABLE orders
-ALTER COLUMN [Profit Per Order] FLOAT;
+ALTER COLUMN [Profit] FLOAT;
 
 
 
@@ -335,7 +337,7 @@ ALTER COLUMN [Sales] FLOAT;
 SELECT DISTINCT [Additional Order items],
 [Category]
 FROM orders
-ORDER BY [Category];
+ORDER BY [Category] ASC;
 
 SELECT DISTINCT [Additional Order items],
 [Category]
@@ -350,7 +352,7 @@ WHERE [Additional Order items] = 'NA';
 
 SELECT DISTINCT [City]
 FROM orders
-ORDER BY [City];
+ORDER BY [City] ASC;
 
 SELECT DISTINCT [City]
 FROM orders
@@ -392,5 +394,483 @@ WHERE [Customer Id] <> [Order Customer Id];
 ALTER TABLE orders
 DROP COLUMN [Additional Order items];
 
+
+
 ALTER TABLE orders
 DROP COLUMN [Order Customer Id];
+
+
+
+---------------------------------------------------------------------------------------------------------------
+
+/* 1.  Total Sales by Year */
+
+SELECT YEAR([Order Date]) AS "Year",
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY YEAR([Order Date])
+ORDER BY [Year] DESC;
+
+
+
+/* 2. Total Quantity by Year */
+
+SELECT YEAR([Order Date]) AS "Year",
+SUM(Quantity) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY YEAR([Order Date])
+ORDER BY [Year] DESC;
+
+
+/* 3. Total Profit by Year */
+
+SELECT YEAR([Order Date]) AS "Year",
+ROUND(SUM([Profit]), 0)  AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY YEAR([Order Date])
+ORDER BY [Year] DESC;
+
+
+
+/* 4. Total Orders by Year */
+
+SELECT YEAR([Order Date]) AS "Year",
+COUNT(DISTINCT [Order Id])  AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY YEAR([Order Date])
+ORDER BY [Year] DESC;
+
+
+
+/* 5. Total Customers by Year */
+
+SELECT YEAR([Order Date]) AS "Year",
+COUNT(DISTINCT [Customer Id])  AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY YEAR([Order Date])
+ORDER BY [Year] DESC;
+
+
+
+/* 6. Year over Year Change for Sales (2017 vs 2016) */
+
+SELECT DISTINCT ROUND(((
+			SELECT SUM([Sales])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2017)
+			-
+			(SELECT SUM([Sales])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016))
+			/
+			(SELECT SUM([Sales])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016)*100, 2) AS "YoY Sales (%)"
+FROM orders;
+
+
+
+/* 7. Year over Year Change for Quantity (2017 vs 2016) */
+
+SELECT DISTINCT ROUND(((
+			SELECT SUM([Quantity])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2017)
+			-
+			(SELECT SUM([Quantity])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016))
+			/
+			(SELECT SUM([Quantity])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016)*100, 2) AS "YoY Quantity (%)"
+FROM orders;
+
+
+
+/* 8. Year over Year Change for Profit (2017 vs 2016) */
+
+SELECT DISTINCT ROUND(((
+			SELECT SUM([Profit])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2017)
+			-
+			(SELECT SUM([Profit])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016))
+			/
+			(SELECT SUM([Profit])
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016)*100, 2) AS "YoY Profit (%)"
+FROM orders;
+
+
+
+/* 9. Year over Year Change for Orders (2017 vs 2016) */
+
+SELECT DISTINCT ROUND((
+			(SELECT CAST(COUNT(DISTINCT [Order Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2017)
+			-
+			(SELECT CAST(COUNT(DISTINCT [Order Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016))
+			/
+			(SELECT CAST(COUNT(DISTINCT [Order Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016)*100 , 1) AS "YoY Orders (%)"
+FROM orders;
+
+
+
+/* 10. Year over Year Change for Customers (2017 vs 2016) */
+
+SELECT DISTINCT ROUND((
+			(SELECT CAST(COUNT(DISTINCT [Customer Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2017)
+			-
+			(SELECT CAST(COUNT(DISTINCT [Customer Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016))
+			/
+			(SELECT CAST(COUNT(DISTINCT [Customer Id]) AS FLOAT)
+			FROM orders
+			WHERE [Country] = 'United States' AND [State] != 'HI' AND YEAR([Order Date]) = 2016)*100, 1) AS "YoY Customers (%)"
+FROM orders;
+
+
+
+/* 11. Sales by Month */
+
+SELECT YEAR([Order Date]) AS "Year",
+DATENAME(MONTH, [Order Date]) AS "Month",
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY MONTH([Order Date]), DATENAME(MONTH, [Order Date]), YEAR([Order Date])
+ORDER BY MONTH([Order Date]), [Year] DESC;
+
+
+
+/* 12. Quantity by Month */
+
+SELECT YEAR([Order Date]) AS "Year",
+DATENAME(MONTH, [Order Date]) AS "Month",
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY MONTH([Order Date]), DATENAME(MONTH, [Order Date]), YEAR([Order Date])
+ORDER BY MONTH([Order Date]), [Year] DESC;
+
+
+
+/* 13. Profit by Month */
+
+SELECT YEAR([Order Date]) AS "Year",
+DATENAME(MONTH, [Order Date]) AS "Month",
+ROUND(SUM([Profit]), 0)  AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY MONTH([Order Date]), DATENAME(MONTH, [Order Date]), YEAR([Order Date])
+ORDER BY MONTH([Order Date]), [Year] DESC;
+
+
+
+/* 14. Orders by Month */
+
+SELECT YEAR([Order Date]) AS "Year",
+DATENAME(MONTH, [Order Date]) AS "Month",
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY MONTH([Order Date]), DATENAME(MONTH, [Order Date]), YEAR([Order Date])
+ORDER BY MONTH([Order Date]), [Year] DESC;
+
+
+
+/* 15. Customers by Month */
+
+SELECT YEAR([Order Date]) AS "Year",
+DATENAME(MONTH, [Order Date]) AS "Month Name",
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY MONTH([Order Date]), DATENAME(MONTH, [Order Date]), YEAR([Order Date])
+ORDER BY MONTH([Order Date]), [Year] DESC;
+
+
+
+/* 16. Sales by State */
+
+SELECT [State],
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [State]
+ORDER BY [State] ASC;
+
+
+
+/* 17. Quantity by State */
+
+SELECT [State],
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [State]
+ORDER BY [State] ASC;
+
+
+
+/* 18. Profit by State */
+
+SELECT [State],
+ROUND(SUM([Profit]), 0) AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [State]
+ORDER BY [State] ASC;
+
+
+
+/* 19. Orders by State */
+
+SELECT [State],
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [State]
+ORDER BY [State] ASC;
+
+
+
+/* 20. Customers by State */
+
+SELECT [State],
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [State]
+ORDER BY [State] ASC;
+
+
+
+/* 21. Sales by Category */
+
+SELECT [Category],
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Category]
+ORDER BY [Total Sales] DESC;
+
+
+
+/* 22. Quantity by Category */
+
+SELECT [Category],
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Category]
+ORDER BY [Total Quantity] DESC;
+
+
+
+/* 23. Profit by Category */
+
+SELECT [Category],
+ROUND(SUM([Profit]), 0) AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Category]
+ORDER BY [Total Profit] DESC;
+
+
+
+/* 24. Orders by Category */
+
+SELECT [Category],
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Category]
+ORDER BY [Total Orders] DESC;
+
+
+
+/* 25. Customers by Category */
+
+SELECT [Category],
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Category]
+ORDER BY [Total Customers] DESC;
+
+
+
+/* 26. Sales by Market */
+
+SELECT [Market],
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Market]
+ORDER BY [Total Sales] DESC;
+
+
+
+/* 27. Quantity by Market */
+
+SELECT [Market],
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Market]
+ORDER BY [Total Quantity] DESC;
+
+
+
+/* 28. Profit by Market */
+
+SELECT [Market],
+ROUND(SUM([Profit]), 0) AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Market]
+ORDER BY [Total Profit] DESC;
+
+
+
+/* 29. Orders by Market */
+
+SELECT [Market],
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Market]
+ORDER BY [Total Orders] DESC;
+
+
+
+/* 30. Customers by Market */
+
+SELECT [Market],
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Market]
+ORDER BY [Total Customers] DESC;
+
+
+
+/* 31. Sales by Region */
+
+SELECT [Region],
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Region]
+ORDER BY [Total Sales] DESC;
+
+
+
+/* 32. Quantity by Region */
+
+SELECT [Region],
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Region]
+ORDER BY [Total Quantity] DESC;
+
+
+
+/* 33. Profit by Region */
+
+SELECT [Region],
+ROUND(SUM([Profit]), 0) AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Region]
+ORDER BY [Total Profit] DESC;
+
+
+
+/* 34. Orders by Region */
+
+SELECT [Region],
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Region]
+ORDER BY [Total Orders] DESC;
+
+
+
+/* 35. Customers by Region */
+
+SELECT [Region],
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Region]
+ORDER BY [Total Customers] DESC;
+
+
+
+/* 36. Sales by Segment */
+
+SELECT [Segment],
+ROUND(SUM([Sales]), 0) AS "Total Sales"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Segment];
+
+
+
+/* 37. Quantity by Segment */
+
+SELECT [Segment],
+SUM([Quantity]) AS "Total Quantity"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Segment];
+
+
+
+/* 38. Profit by Segment */
+
+SELECT [Segment],
+ROUND(SUM([Profit]), 0) AS "Total Profit"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Segment];
+
+
+
+/* 39. Orders by Segment */
+
+SELECT [Segment],
+COUNT(DISTINCT [Order Id]) AS "Total Orders"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Segment];
+
+
+
+/* 40. Customers by Segment */
+
+SELECT [Segment],
+COUNT(DISTINCT [Customer Id]) AS "Total Customers"
+FROM orders
+WHERE [Country] = 'United States' AND [State] != 'HI'
+GROUP BY [Segment];
